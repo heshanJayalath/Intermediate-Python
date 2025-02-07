47f29a47671b8f466014cd0f1f4b2e08aca9efcc
Malinga Heshan <heshanjayalath66@gmail.com>
2025-02-08 00:58:53 +0530

    updated user input

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -24,3 +24,27 @@ MENU ={
     },
 }
 
+resource = {
+    "water":300,
+    "milk":200,
+    "coffee":100
+}
+
+def calculate_resources():
+    return ""
+
+def generate_report():
+    for key,value in resource.items():
+        print(f"{key} : {value}")
+
+user_input = True
+while user_input:
+    user_input = input("What would you like? (espresso/latte/cappuccino):")
+    match user_input:
+        case "espresso" | "latte" | "cappuccino":
+            print(f"{user_input}")
+        case "report":
+            calculate_resources()
+        case "off":
+            user_input = False
+
