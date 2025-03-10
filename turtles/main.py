40c063692363d016284cfc6bac73730c79ee5b85
Malinga Heshan <heshanjayalath66@gmail.com>
2025-03-10 14:14:18 +0530

    oop

---
+++ turtles/main.py
@@ -0,0 +1,7 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("City name",
+["Adelaide","Brisbane","Darwin","Hobart","Sydney","Melbourne","Perth"])
+table.add_column("Area", [1295, 5905, 112, 1357, 2058, 1566, 5386])
+print(table)
